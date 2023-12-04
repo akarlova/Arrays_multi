7f52759013db1a047a098b5388ce8f167944ecad
aka.rlova <ladyerial88@gmail.com>
2023-12-04 23:19:50 +0200

    Basic solution update

--- src/app/Main.java
+++ src/app/Main.java
@@ -1,11 +1,10 @@
 package app;
 
-import java.util.Arrays;
 import java.util.Scanner;
 
 public class Main {
 
-    static int luckyNum;
+    private static final int WIN_NUM = 61;
     static int count;
 
     public static void main(String[] args) {
@@ -13,22 +12,24 @@ public class Main {
                 {15, 25, 34, 41},
                 {37, 78, 24, 13},
                 {20, 30, 44, 55},
-                {18, 61, 72, 80}};
-
+                {18, 61, 72, 80}
+        };
+        boolean isWin = false;
         Scanner sc = new Scanner(System.in);
 
         System.out.println("Please, enter your number ");
-        int number = sc.nextInt();
+        int choice = sc.nextInt();
 
         for (int[] rows : happyNumbers) {
             for (int value : rows) {
-                if (value == number) {
-                    count++;
+                if (value == WIN_NUM && value == choice) {
+                    isWin = true;
+                    break;
                 }
             }
         }
-        if (count > 0) {
-            System.out.printf("Yes, %d is lucky!", number);
+        if (isWin) {
+            System.out.printf("Yes, %d is lucky!", choice);
         } else {
             System.out.println("Sorry, try again!");
         }
